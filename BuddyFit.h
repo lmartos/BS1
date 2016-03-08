fce9208c4901fc072cae900940cf74a168340cec
lmartos <l-martos@hotmail.com>
2016-03-08 13:40:52 +0100

    started work on buddyfit

--- BuddyFit.h
+++ BuddyFit.h
@@ -1,17 +1,22 @@
 #ifndef BUDDYFIT_H
 #define BUDDYFIT_H
+#include <string>
+#include "Area.h"
+#include "Fitter.h"
+using namespace std;
 
 
-class BuddyFit : public Fitter
+class BuddyFit : public Area
 {
 	public:
-		BuddyFit();
+		BuddyFit(string name, int base, int size);
 		~BuddyFit();
-		public Area *alloc(int wanted);
-		public void  free(Area *);
-		public void  report();
+		Area *alloc(int wanted);
+		void  free(Area *);
 	protected:
 	private:
+		void split();
+		string name;
 		/// Pointer to the left buddy in the tree
 		BuddyFit *buddyFitLeft;
 		/// Pointer to the right buddy in the tree
